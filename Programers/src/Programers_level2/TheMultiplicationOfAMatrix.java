8862b6027e90def34a037fa856c6e358178d9ffe
yahoo <yahoo@LAPTOP-1HVBBQLT>
2022-09-15 20:19:26 +0900

    프로그래머스 level2 행렬의 곱셈 commit

---
+++ Programers/src/Programers_level2/TheMultiplicationOfAMatrix.java
@@ -0,0 +1,25 @@
+package Programers_level2;
+
+public class TheMultiplicationOfAMatrix {
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr2[0].length;j++){
+                for(int t=0;t<arr1[0].length;t++){
+                    answer[i][j] += (arr1[i][t] * arr2[t][j]);
+                }
+            }
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		int[][] a = {{1,4},{3,2},{4,1},{2,3,2},{4,2,4},{3,1,4}};
+		int[][] b = {{3,3},{3,3},{5,4,3},{2,4,1},{3,1,1}};
+		
+		TheMultiplicationOfAMatrix s = new TheMultiplicationOfAMatrix();
+		
+		s.solution(a,b);
+	}
+}
